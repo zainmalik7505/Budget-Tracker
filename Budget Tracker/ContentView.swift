5b296fcdd10ebc7ec6039be567d11c12d9abc04b
Zain Malik <xaineil121@gmail.com>
2024-08-27 14:59:44 +0500

    Initial Commit

---
+++ Budget Tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Budget Tracker
+//
+//  Created by Zain Malik on 27/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
